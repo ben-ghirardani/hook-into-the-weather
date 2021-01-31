70d3c6ca75be66b935ee10dcff467f0d0bd8d654
Ben <pretty.fiendish@gmail.com>
2021-01-31 19:34:53 +0000

    started work on search box functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div>
       {test}
+      {/* search box */}
     </div>
   );
 }
---
+++ src/components/SearchBox/SearchBox.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from 'react';
+import './SearchBox.css';
+
+export default function SearchBox() {
+
+  const [ text, setText ] = useState("");
+
+  return (
+    <div>
+      {/* text box, onChange updates text variable which is matched against the array of locations to produce an autofill list */}
+    </div>
+  )
+
+};
