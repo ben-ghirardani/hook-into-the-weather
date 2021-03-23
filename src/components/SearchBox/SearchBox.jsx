0e00ce3111f01853de1c8f7e3c0973420fb4f0ed
Ben <pretty.fiendish@gmail.com>
2021-03-23 18:19:45 +0000

    added initial functionality to onSubmit

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -7,8 +7,6 @@ export default function SearchBox() {
   const [ text, setText ] = useState("");
   const [ woeid, setWoeid ] = useState("");
 
-  const countyData = CountryData;
-
   const onChange = (e) => {
     e.preventDefault();
     setText(e.currentTarget.value);
@@ -17,10 +15,16 @@ export default function SearchBox() {
   const submitText = (e) => {
     e.preventDefault();
     console.log("add functionality here!")
+    // loop through countryData
+    const countryData = CountryData;
+    countryData.forEach(item => {
+      if (item.Location === text) {
+        console.log("there's a match")
+      } else {
+        console.log("no match :(")
+      }
+    });
   };
-
-  // function to check current text against array of locations
-  // call it in onChange above
 
   // is there too much happening in SearchBox? Not reusable. Pull some of the functionality out and into app?
 
