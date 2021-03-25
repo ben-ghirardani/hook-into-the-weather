f48e0a79c3199d068557e0556e0ef763599a58c5
Ben <pretty.fiendish@gmail.com>
2021-03-25 20:20:43 +0000

    moving WOEID functionality from SearchBox to main App to make SearchBox reusable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage.jsx';
-import useFetch from './components/useFetch.jsx';
+import useFetch from './components/useFetch/useFetch.jsx';
 
 function App() {
 
