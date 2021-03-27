4a6cae9d2bcba97e3871f7a85dd29a693098171d
Ben <pretty.fiendish@gmail.com>
2021-03-27 11:26:01 +0000

    re-implemented useFetch and pqssed function to pull location to top of tree

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import './App.css';
 import LandingPage from './components/LandingPage/LandingPage.jsx';
+import useFetch from './components/UseFetch/UseFetch.jsx';
 
 
 function App() {
   const [ locationData, setLocationData ] =  useState("");
 
   // takes a woeid, formats a url, passes it to useFetch, and fetches the data
-  // function getLocation(woeid) {
-  //   let url = `https://www.metaweather.com/api/location/${woeid}/`
-  //   // let data = UseFetch(url);
-  //   let data = <Usedata/>
-  //   useState(data)
-  // }
+  function getLocation(woeid) {
+    let url = `https://www.metaweather.com/api/location/${woeid}/`
+    let data = useFetch(url);
+    useState(data);
+  }
 
   return (
     <>
       <LandingPage
-        // getLocation={getLocation}
+        getLocation={getLocation}
       />
     </>
   );
